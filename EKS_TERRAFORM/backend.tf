9d0ac58417afc75843a1bd98cf7a362a831edad6
Amul Thantharate <89029777+Amul-Thantharate@users.noreply.github.com>
2024-01-29 15:48:49 +0530

    Update backend configuration in Terraform
    This commit updates the backend configuration in the Terraform file `backend.tf`. The S3 bucket name, S3 key name, S3 bucket region, and DynamoDB table name have been modified.

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "terraform-backend-bucket-12" # Replace with your actual S3 bucket name
-    key    = "Hotstar/terraform.tfstate"
-    region = "ap-northeast-1"
+    key    = "Hotstar/terraform.tfstate" # Replace with your actual S3 key name
+    region = "ap-northeast-1" # Replace with your S3 bucket region
+    dynamodb_table = "terraform-state-lock" # Replace with your actual DynamoDB table name
   }
 }
