897c29e0c2a81704e4aeea9b2ca55e09d7747010
Amul Thantharate <89029777+Amul-Thantharate@users.noreply.github.com>
2024-03-20 19:08:19 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-backend-bucket-12" # Replace with your actual S3 bucket name
-    key    = "Hotstar/terraform.tfstate" # Replace with your actual S3 key name
-    region = "ap-northeast-1" # Replace with your S3 bucket region
-    dynamodb_table = "terraform-state-lock" # Replace with your actual DynamoDB table name
-  }
+    backend "s3" {
+        bucket = "terraform-state-bucket-122"
+        key    = "Brainwave/terraform.tfstate"
+        region = "us-east-1"
+        encrypt = true
+    }
 }
